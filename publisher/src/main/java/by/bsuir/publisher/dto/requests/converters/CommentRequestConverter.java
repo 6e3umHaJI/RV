75952e753e603ce53874cf1dedc21a755ba2c39d
Aryna Baranouskaya <baranovskaia.arina@gmail.com>
2025-04-26 02:29:00 +0300

    init commit

--- publisher/src/main/java/by/bsuir/publisher/dto/requests/converters/CommentRequestConverter.java
+++ publisher/src/main/java/by/bsuir/publisher/dto/requests/converters/CommentRequestConverter.java
@@ -3,8 +3,10 @@ package by.bsuir.publisher.dto.requests.converters;
 import by.bsuir.publisher.domain.Comment;
 import by.bsuir.publisher.dto.requests.CommentRequestDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CommentRequestConverter {
+    @Mapping(source = "topicId", target = "topic.id")
     Comment fromDto(CommentRequestDto comment);
 }
